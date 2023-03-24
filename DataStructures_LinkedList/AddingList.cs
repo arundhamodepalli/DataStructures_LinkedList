29ecde9459711aca49b2f98b86d15ff8885d1704
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-24 16:13:09 +0530

    Adding List in  datastructure linked list

---
+++ DataStructures_LinkedList/AddingList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_LinkedList
+{
+    class AddingList
+    {
+        public int data;
+        public Node next;
+
+        public AddingList(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    class NodeList
+    {
+        Node head;
+
+        public void AddNode(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node current = head;
+
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+
+        public void PrintList()
+        {
+            Node current = head;
+
+            while (current != null)
+            {
+                Console.Write(current.data + " ");
+                current = current.next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- DataStructures_LinkedList/Program.cs
+++ DataStructures_LinkedList/Program.cs
@@ -6,11 +6,20 @@
         {
             //Console.WriteLine("welcome to datastructures linkedlist");
 
+            //LinkedList myList = new LinkedList();
+
+            //myList.AddNode(56);
+            //myList.AddNode(30);
+            //myList.AddNode(70);
+
+            //myList.PrintList();
+
+
             LinkedList myList = new LinkedList();
 
-            myList.AddNode(56);
-            myList.AddNode(30);
-            myList.AddNode(70);
+            myList.AddNode(70); // first node created
+            myList.AddNode(30); // added to the first node
+            myList.AddNode(56); // added to the second node
 
             myList.PrintList();
         }
