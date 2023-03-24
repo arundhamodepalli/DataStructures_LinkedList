b0f8af3ba291ead541f5236f11b1a3bda5a2c22a
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-24 16:04:07 +0530

    simple List in  datastructure linked list

--- DataStructures_LinkedList/Program.cs
+++ DataStructures_LinkedList/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to datastructures linkedlist");
+            //Console.WriteLine("welcome to datastructures linkedlist");
+
+            LinkedList myList = new LinkedList();
+
+            myList.AddNode(56);
+            myList.AddNode(30);
+            myList.AddNode(70);
+
+            myList.PrintList();
         }
     }
 }
