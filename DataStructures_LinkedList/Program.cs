733bf18ca86f4d0f603d444574f6886d3d6eae58
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-24 15:58:42 +0530

    welcome to datastructure linked list

---
+++ DataStructures_LinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructures_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to datastructures linkedlist");
+        }
+    }
+}
